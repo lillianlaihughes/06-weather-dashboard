3abf0c92fef05b7dcac90ba13afdee6952eb7187
Lillian Lai Hughes <lillian@lillians-air.attlocal.net>
2023-03-06 15:28:10 -0500

    updated html and js

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,4 @@
-# 06-weather-dashboard
-
-## User Story
-
-AS A traveler
-I WANT to see the weather outlook for multiple cities
-SO THAT I can plan a trip accordingly
-
-## Acceptance Criteria
-
+/*
 GIVEN a weather dashboard with form inputs
 WHEN I search for a city
 THEN I am presented with current and future conditions for that city and that city is added to the search history
@@ -17,3 +8,24 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+*/
+
+// create an input field for city search
+// store city in search history (local storage)
+
+// display current forecast
+// including city, date, weather icon, temperature, humidity, and wind speed in forecast results
+    // request data from API
+    // check that data has value
+    // parse that data into chunks to display
+
+
+// display 5-day forecast
+// including future dates, weather icon, temperature, humidity, and wind speed in forecast results
+    // request data from API
+    // check that data has value
+    // parse that data into chunks to display
+/ 
+// stored cities in search history are displayed as links (or buttons?)
+
+// when those cities are clicked, the forecast results are displayed again
