1dea8f60dceead31cfba1bc2b6191d69c0ac457d
lillianlaihughes <lillianlaihughes@gmail.com>
2023-07-24 14:21:56 -0400

    updated index, js, css

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,5 @@
-/*
-GIVEN a weather dashboard with form inputs
-WHEN I search for a city
-THEN I am presented with current and future conditions for that city and that city is added to the search history
-WHEN I view current weather conditions for that city
-THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the the wind speed
-WHEN I view future weather conditions for that city
-THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-WHEN I click on a city in the search history
-THEN I am again presented with current and future conditions for that city
-*/
-
-/* NOTE TO SELF: use OpenWeather API, DayJS, and jQuery 
--- choose Bootstrap or MUI for css?
-*/
-
 // create an input field for city search
 // store city in search history (local storage)
-var citySearchInput = $("")
-var localStorage =
-
-// display current day
-var currentDay = dayjs().format("dddd, MMMM D YYYY");
-
-var cardDay1 = 
-
 // display current forecast
 // including city, date, weather icon, temperature, humidity, and wind speed in forecast results
 // request data from API
@@ -39,3 +15,55 @@ var cardDay1 =
 // stored cities in search history are displayed as links (or buttons?)
 
 // when those cities are clicked, the forecast results are displayed again
+
+var apiKey = "58abc759a772fbab2d31d643044911b5"
+var citySearchInput = $("")
+var localStorage =
+
+// display current day
+var currentDay = dayjs().format("dddd, MMMM D YYYY");
+var cardDay1 = 
+
+function getWeather() {
+  var cityName = document.getElementById("citySearchInput").value
+  var geoApi = "https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=" + apiKey
+
+  fetch(geoApi)
+      .then(function (response) {
+          return response.json()
+      })
+
+      .then(function (data) {
+          console.log(data)
+          cityLat = data[0].lat
+          console.log(cityLat, "CITY LAT VALUE")
+          cityLon = data[0].lon
+          console.log(cityLon, "CITY LON VALUE")
+
+          searchedCity = {
+              Name: cityName,
+              Lat: cityLat,
+              Lon: cityLon,
+          }
+
+          console.log(searchedCity, "SC Value")
+
+          searchHistory.push(searchedCity)
+          console.log(searchHistory, "SEARCH History Value")
+
+          window.localStorage.setItem(searchHistory, JSON.stringify(searchHistory))
+          
+          var weatherApi = "https://api.openweathermap.org/data/2.5/forecast?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&appid=" + apiKey
+
+          fetch(weatherApi)
+          .then(function (response) {
+              return response.json()
+          })
+  
+          .then(function (data) {
+  
+          })
+
+      })
+
+    };
